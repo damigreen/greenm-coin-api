d12c97fc59c3b02b47fbc878b1147847681ff7eb
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-11 14:43:20 +0100

    add mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const config = require('./utils/config')
+const mongoUrl = config.MONGO_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
+  .then(() => console.log('Connected to mongoDB'))
+  .catch(err => console.log(err));
+
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+/* 
+* Todo
+Create logic for handling users
+  Create user
+  delete user
+  Get user
+  update user
+*/
