a4df78dc5c38aaffa920be9a1cb7eacd58c39789
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-11 21:06:55 +0100

    create login route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -9,5 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Defined Routes
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
---
+++ src/routes/login.js
@@ -0,0 +1,32 @@
+const loginRouter = require('express').Router();
+const User = require('../models/user');
+const config = require('../utils/config');
+const jwt = require('jsonwebtoken');
+
+const saltRounds = 10;
+
+loginRouter.post('/', async (req, res, next) => {
+  try {
+    const body = req.body;
+  
+    const user = await User.findOne({ email: body.email });
+    passwordCorrect = user === null ? false : await bcrypt.compare(user.passwordHash, body.password);
+  
+    if (!(user && passwordCorrect)) {
+      return res.sendStatus(401).json({ error: 'invalid email or password'});
+    }
+  
+    const userForToken = {
+      email: user.email,
+      id: user._id,
+    };
+  
+    const token = jwt.sign(userForToken, config.SECRET);
+    await res.sendStatus(200).send(token, { name: user.name, email: user.email });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+module.exports = loginRouter;
