b4377ca2a0759d6154d64b8836105a4a4cbffc42
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-11 14:43:33 +0100

    add user route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const signupRouter = require('./routes/signup');
+const usersRouter = require('./routes/users');
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Defined Routes
-app.use('/api/signup', signupRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
