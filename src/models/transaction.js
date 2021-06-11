b6cf31c2844d76c7e443717357e8314dd8e186e3
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-11 19:07:10 +0100

    Add transaction to db

---
+++ src/models/transaction.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = mongoose.Schema({
+  transactionType: { type: String },
+  transactionAccount: { type: String },
+  date: { type: Date },
+  amount: { type: Number },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
