2af7f32138f8319fe5e38e1be63fa2a7bd0c6d3f
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-11 21:05:28 +0100

    add jwt secret

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 let MONGO_URI = process.env.MONGO_URI;
 let PORT = process.env.PORT;
-console.log(PORT);
+const SECRET = process.env.SECRET;
 
 module.exports = {
   MONGO_URI,
   PORT,
+  SECRET,
 }
