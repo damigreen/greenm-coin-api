de947b5496e45502089f446eaa7a8adea57bcda1
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-12 10:48:26 +0100

    add new users

--- src/mongo.js
+++ src/mongo.js
@@ -9,63 +9,68 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
 
 
 const userSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  number: { type: String, require: true },
+  name: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  number: { type: String, require: true, unique: true },
+  balance: { type: Number },
+  transactions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Transaction'
+    }
+  ]
 });
-// userSchema.plugin(uniqueValidator);
 
-// userSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString();
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//   }
-// });
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
 
 
 const User = mongoose.model('User', userSchema);
 
 
 
-try {
-  // const user = new User({
-  //   name: 'Damilola Faseun',
-  //   email: 'fashfired@gmail.com',
-  //   number: '09017755801',
-  // });
-
-
-  // user.save().then((result) => {
-  //   console.log('user saved');
-  //   mongoose.connection.close()
-  // });
-
-  // const users = [
-  //   {
-  //     name: 'Johny King',
-  //     email: 'johnyking@gmail.com',
-  //     number: '09010939801',
-  //   },
-  //   {
-  //     name: 'Yemi Flash',
-  //     email: 'yemiflash@gmail.com',
-  //     number: '09010939038',
-  //   },
-  //   {
-  //     name: 'Bruce Banners',
-  //     email: 'brucebanners@gmail.com',
-  //     number: '09013889801',
-  //   },
-  // ]
-
-  // User.insertMany(users, (err, result) => {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(result);
-  //   }
-  // });
+try {  
+  const users = [
+    {
+      name: 'Damilola Faseun',
+      email: 'fashfired@gmail.com',
+      number: '09017755801',
+      password: 'password'
+    },
+    {
+      name: 'Johny King',
+      email: 'johnyking@gmail.com',
+      number: '09010939801',
+      password: 'password'
+    },
+    {
+      name: 'Yemi Flash',
+      email: 'yemiflash@gmail.com',
+      number: '09010939038',
+      password: 'password'
+    },
+    {
+      name: 'Bruce Banners',
+      email: 'brucebanners@gmail.com',
+      number: '09013889801',
+      password: 'password'
+    },
+  ]
+  
+  User.insertMany(users, (err, result) => {
+    if (err) {
+      console.log(err);
+      // mongoose.connection.close()
+    } else {
+      console.log(result);
+    }
+  });
 
   
 } catch (e) {
