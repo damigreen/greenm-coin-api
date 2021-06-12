fd2c12a68088dede8d2b6caf4d2f6200c65239b1
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-12 22:42:43 +0100

    add error handler and logger middleware

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ const {
   debitRouter,
   transactionRouter,
 } = require('./routes/transactions');
-// const fundRouter = require('./routes/transactions');
+const middleware = require('./utils/middleware');
+
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(middleware.requestLogger);
 
 // Defined Routes
 app.use('/api/users', usersRouter);
@@ -20,5 +22,7 @@ app.use('/api/login', loginRouter);
 app.use('/api/transactions/credit', creditRouter);
 app.use('/api/transactions/debit', debitRouter);
 app.use('/api/transactions', transactionRouter);
+
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ src/utils/middleware.js
@@ -0,0 +1,25 @@
+const requestLogger = (request, response, next) => {
+  console.info(`Method: ${request.path}`);
+  console.info(`Path: ${request.path}`);
+  console.info(`Body: ${request.body}`);
+  console.info('----');
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'Malformated ID'});
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json(error.message);
+  } else if (error.name === 'jsonWebTokenError') {
+    return response.status(401).send({ error: 'invalid token'});
+  }
+
+  console.error(error.message);
+  next(error);
+}
+
+module.exports = {
+  requestLogger,
+  errorHandler,
+}
