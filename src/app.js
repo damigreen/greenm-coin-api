9c6954052bba84b112575acdb682e3dd97bb440c
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-14 07:39:56 +0100

    App to serve static build file

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const middleware = require('./utils/middleware');
 
 // Body parser middleware
 app.use(cors());
+app.use(express.static('build'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(middleware.requestLogger);
